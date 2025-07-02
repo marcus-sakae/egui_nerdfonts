2fa78220a3dae92450e81e2d322ce4b73dfca726
Marcus <kumorig@gmail.com>
2025-07-02 14:44:41 +0200

    bump to egui 0.31.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
+use std::sync::Arc;
+
 pub mod variants;
 pub use variants::*;
 
 pub fn add_to_fonts(fonts: &mut egui::FontDefinitions, variant: Variant) {
     fonts
         .font_data
-        .insert("nerdfonts".into(), variant.font_data());
+        .insert("nerdfonts".into(), Arc::new(variant.font_data()));
 
     if let Some(font_keys) = fonts.families.get_mut(&egui::FontFamily::Proportional) {
         font_keys.push("nerdfonts".into());
